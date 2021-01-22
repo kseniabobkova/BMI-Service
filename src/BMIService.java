d20af4a768f0cf26ddc8589b1704d9b07efb9528
kseniabobkova <bobkova@healbe.com>
2021-01-22 13:10:16 +0300

    BMI2

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,7 +1,7 @@
 public class BMIService {
     public String calculate(double waightKg, double growthM) {
         double index = waightKg / (growthM * growthM);
-        String result = String.format("%.2f",index);
+        String result = String.format("%.2f", index);
         return result;
 
     }
