44c641834b27cb18e5b0a3be05ed1d6cadd13473
kseniabobkova <bobkova@healbe.com>
2021-01-25 12:23:09 +0300

    BMI5

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,9 +1,8 @@
 public class BMIService {
     public double calculate(double waightKg, double growthM) {
-        double index = waightKg / (growthM * growthM) * 100;
-        long index2 = Math.round(index);
-        double indexFin = (double) index2 / 100;
-        return indexFin;
+        double index = waightKg / (growthM * growthM);
+        long indexRound = Math.round(index);
+        return indexRound;
 
     }
 }
